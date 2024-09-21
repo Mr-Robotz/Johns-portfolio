0f39ab0309504574c350f19b3d2a493a4c5c3113
Mr Robotz <mrr.robotz@gmail.com>
2024-09-20 19:38:50 -0700

    added the .map() key to the nav-link.tsx component

--- components/ui/nav-links.tsx
+++ components/ui/nav-links.tsx
@@ -27,9 +27,8 @@ function NavLinks() {
     <div className="hidden sm:block">
       <nav className="mx-6 md:flex items-center space-x-4 lg:space-x-6 hidden text-gray-900">
               {routes.map((route, i) => (
-                <Button asChild variant="ghost">
+                <Button asChild variant="ghost" key={i}>
                   <Link
-                    key={i}
                     href={route.href}
                     className="text-sm font-medium transition duration-300 hover:text-blue-700"
                   >
