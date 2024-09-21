ebbc72250a16143fcb428a13ce7e248eaabc6d76
Mr Robotz <mrr.robotz@gmail.com>
2024-09-20 19:48:23 -0700

    Wrapped the Button in the nav-lin.tsx components with the div and gave it the .map() Key

--- components/ui/nav-links.tsx
+++ components/ui/nav-links.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-
 const routes = [
   {
     href: "/",
@@ -21,21 +20,22 @@ const routes = [
   },
 ];
 
-
 function NavLinks() {
   return (
     <div className="hidden sm:block">
       <nav className="mx-6 md:flex items-center space-x-4 lg:space-x-6 hidden text-gray-900">
-              {routes.map((route, i) => (
-                <Button asChild variant="ghost" key={i}>
-                  <Link
-                    href={route.href}
-                    className="text-sm font-medium transition duration-300 hover:text-blue-700"
-                  >
-                    {route.label}
-                  </Link>
-                </Button>
-              ))}
+        {routes.map((route, i) => (
+          <div key={i}>
+            <Button asChild variant="ghost">
+              <Link
+                href={route.href}
+                className="text-sm font-medium transition duration-300 hover:text-blue-700"
+              >
+                {route.label}
+              </Link>
+            </Button>
+          </div>
+        ))}
       </nav>
     </div>
   );
